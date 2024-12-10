fa0af14945261f8e2c8da16c3648dc107aa91709
Ezee27 <zaenalgaming48@gmail.com>
2024-12-10 10:07:40 -0500

    view

--- mahasiswa.py
+++ mahasiswa.py
@@ -1,30 +1,15 @@
-class Mahasiswa:
-    def __init__(self, nim, nama, jurusan):
-        self.nim = nim
-        self.nama = nama
-        self.jurusan = jurusan
+class ViewMahasiswa:
+    @staticmethod
+    def tampilkan_semua_mahasiswa(data):
+        if not data:
+            print("Tidak ada data mahasiswa.")
+        else:
+            for m in data:
+                print(f"NIM: {m.nim}, Nama: {m.nama}, Jurusan: {m.jurusan}")
 
-class DataMahasiswa:
-    def __init__(self):
-        self.data = []
-
-    def tambah_mahasiswa(self, nim, nama, jurusan):
-        mahasiswa = Mahasiswa(nim, nama, jurusan)
-        self.data.append(mahasiswa)
-
-    def hapus_mahasiswa(self, nim):
-        self.data = [m for m in self.data if m.nim != nim]
-
-    def ubah_mahasiswa(self, nim, nama_baru=None, jurusan_baru=None):
-        for m in self.data:
-            if m.nim == nim:
-                if nama_baru:
-                    m.nama = nama_baru
-                if jurusan_baru:
-                    m.jurusan = jurusan_baru
-
-    def cari_mahasiswa(self, nim):
-        return next((m for m in self.data if m.nim == nim), None)
-
-    def semua_mahasiswa(self):
-        return self.data
+    @staticmethod
+    def tampilkan_mahasiswa(mahasiswa):
+        if mahasiswa:
+            print(f"NIM: {mahasiswa.nim}, Nama: {mahasiswa.nama}, Jurusan: {mahasiswa.jurusan}")
+        else:
+            print("Mahasiswa tidak ditemukan.")
